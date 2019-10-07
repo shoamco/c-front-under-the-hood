0018d92c3035d3a462395ae2c942185249902c0a
shoamco <shoamc98@gmail.com>
2019-10-06 23:32:02 -0700

    finsh phase 2

--- cpp2c_inheritance_defs.h
+++ cpp2c_inheritance_defs.h
@@ -19,28 +19,6 @@ typedef struct Materials
 {
 }Materials;
 
-/*const char* getName(Types type);
-
-const char* getName(Types type)
-{
-
-    const char* const names[] = { "Plastic", "Metal", "Wood", "Paper", "Other" };
-    return names[type];
-}
- */
-
-
-/*
-struct Material_t: public Materials
-{
-    Material_t(Types mat = OTHER): material(mat) { printf("Material created, set to %s\n", name()); }
-    const char* name() const { return getName(material); }
-
-    Types material;
-};*/
-
-
-
 
 typedef struct  Material_t{
     /*inheritance Materials-empty*/
@@ -66,58 +44,7 @@ void __PhysicalBox_CTOR_d_d_d_t__(PhysicalBox *const this,double l, double w, do
 void __PhysicalBox_CTOR_t__(PhysicalBox *const this ,Types t);
 void __PhysicalBox_DTOR__(PhysicalBox *const this);
 void __printp_cpc__(const PhysicalBox *const this) ;
-/*
-class PhysicalBox: public Box
-{
-public:
-    PhysicalBox(double l, double w, double h);
-    PhysicalBox(double l, double w, double h, Materials::Types t);
-    PhysicalBox(Materials::Types t);
-    ~PhysicalBox();
 
-    Materials::Types getMaterial() const;
-    void printp() const;
-
-private:
-    Material_t material;
-};
-
-
-
-//// PhysicalBox Defs ////////////
-
-inline Materials::Types PhysicalBox::getMaterial() const
-{
-    return material.material;
-}
-
-inline bool operator==(const PhysicalBox& lhs, const PhysicalBox& rhs)
-{
-    return (const Box&)lhs == (const Box&)rhs && lhs.getMaterial() == rhs.getMaterial();
-}
-
-inline bool operator!=(const PhysicalBox& lhs, const PhysicalBox& rhs)
-{
-    return !(lhs == rhs);
-}
-*/
-/*/// WeightBox ////////////*/
-/*class WeightBox: public Box
-{
-public:
-    WeightBox(double l, double w, double h, double wgt = 0.0);
-    WeightBox(const WeightBox& other);
-    ~WeightBox();
-
-    WeightBox& operator=(const WeightBox& other);
-
-    double getWeight() const;
-    void printw() const;
-
-private:
-    double weight;
-};
-*/
 typedef struct WeightBox{
     /*inheritance Box*/
     Box box;
@@ -131,30 +58,7 @@ void __WeightBox_DTOR__(WeightBox *this);
 WeightBox __WeightBox__operator_Assignment_p_p (WeightBox *const this,WeightBox *const other);
 void __WeightBox_printw__(const WeightBox *const this);
 
-/*
-bool operator==(const WeightBox&, const WeightBox&);
-bool operator!=(const WeightBox&, const WeightBox&);
 
-//// WeightBox Defs ////////////
-
-inline double WeightBox::getWeight() const
-{
-    return weight;
-}
-
-inline bool operator==(const WeightBox& lhs, const WeightBox& rhs)
-{
-    return (const Box&)lhs == (const Box&)rhs && lhs.getWeight() == rhs.getWeight();
-}
-
-inline bool operator!=(const WeightBox& lhs, const WeightBox& rhs)
-{
-    return !(lhs == rhs);
-}
-
-
-
-*/
 
 
 
